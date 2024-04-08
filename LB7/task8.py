a3a22fe280712438507b7f1e31dbbb0ce7d8b1e3
rabbitwhoi <151567849+rabbitwhoi@users.noreply.github.com>
2024-04-08 10:54:25 +0300

    Update task8.py

--- LB7/task8.py
+++ LB7/task8.py
@@ -1,6 +1,9 @@
-
 numbers = [1, 2, 3, 4, 5]
 
-sum_of_numbers = sum(numbers)
+sum = 0
 
-print("Сумма всех элементов списка:", sum_of_numbers)
+
+for i in range(len(numbers)):
+    sum += numbers[i] 
+
+print("Сумма всех элементов списка:", sum)
