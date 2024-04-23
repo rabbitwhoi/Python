067b1854759b4be0f03c2c64d4bf913b7953afd7
rabbitwhoi <151567849+rabbitwhoi@users.noreply.github.com>
2024-04-23 23:52:08 +0300

    Update task_1.py

--- LB8/task_1.py
+++ LB8/task_1.py
@@ -194,3 +194,88 @@ print(" ")
 
 print("Рассказ компьютера: ")
 print("Привет! Меня зовет", quest2, "я из города", quest4, ", мое любимое животное", quest6, ", мое хобби это", quest8, ", я по професси ", quest10, "я бы хотел побывать в", quest12)
+
+
+''' import random 
+
+
+s = ["Артем", "Людвиг", "Амстердам", "Александр", "Алексей", "Париж", "Тигр", "Красные волосы", "Плавание", "Врач", "Токио", "Елена", "Лондон", "Собака", 
+     "Серфинг", "Программист", "Москва", "Максим", "Нью-Йорк", "Лев", "Катание на велосипеде", "Учитель", "Берлин", "Юлия", "Барселона", 
+     "Кот", "Пение", "Актер", "Рим", "Джон", "Сан-Франциско", "Зебра", "Фотография", "Инженер", "Ирина", "Чикаго", "Попугай", "Блондинка", "Гитара", "Пилот", "Мадрид", 
+     "Сергей", "Дубай", "Слон", "Рисование", "Юрист", "Милан", "Виктория", "Сидней", "Лиса", "Йога", "Архитектор", "Амстердам", "Вадим", "Канкун", "Панда", "Скейтбординг", 
+     "Фотограф", "Венеция", "Анна", "Мумбаи", "Кролик", "Бег", "Дизайнер", "Каир", "Марина", "Торонто", "Обезьяна", "Бальные танцы", "Бухгалтер", "Лас-Вегас", "Екатерина", 
+     "Шанхай", "Гепард", "Лыжный спорт", "Биолог", "Стамбул", "Дмитрий", "Хельсинки", "Жираф", "Танцы живота", "Стоматолог", "Афины", "Ева"]
+
+questions = ['Как тебя зовут?', 'Твоя места', 'Спорт какой', 'Какое хобби', 'Какое домашнее животное']
+
+player_history = []
+pc_history = []
+
+current_index = 0
+current_index2 = 0
+
+def catch_ball():
+    if random.random() <= 0.3:
+        return True
+    else:
+        return False
+
+def plaing_game(current_index):
+    for i in range(1, 6):
+        if catch_ball():
+            print('Компьютер бросил мяч, Вы поймали!')
+            quest = questions[current_index]
+            print('Вопрос: ', quest)
+            answer = input('Введите ответ: ')
+            print('Ответ: ', answer)
+            print(' ')
+            player_history.append(answer)
+            current_index =+ i
+        else:
+            print('Компьютер бросил мяч, Вы НЕ поймали!')
+            quest = questions[current_index]
+            print('Вопрос: ', quest)
+            answer = random.choice(s)
+            print('Ответ: ', answer)
+            print(' ')
+            player_history.append(answer)
+            current_index =+ i
+
+plaing_game(current_index)
+
+print('--------------------------------------------------------')
+print('Рассказ меня: ')
+print('Меня зовут', player_history[0], 'я живу в', player_history[1], 'я занимаюсь', player_history[2], 'мое хобби это', player_history[3], 'мое домашнее животное это', 
+      player_history[4])
+print('--------------------------------------------------------')
+print(' ')
+
+def plaing_player_comp(current_index2):
+    for r in range(1, 6):
+        if catch_ball():
+            print('Вы бросили мяч, Комп поймал!') 
+            quest = questions[current_index2]
+            print('Вопрос: ', quest)
+            answer = random.choice(s)
+            print('Ответ: ', answer)
+            print(' ')
+            pc_history.append(answer)
+            current_index2 =+ r
+        else:
+            print('Вы бросили мяч, Комп НЕ поймал!')
+            quest = questions[current_index2]
+            print('Вопрос: ', quest)
+            answer = input('Введите ответ: ')
+            print('Ответ: ', answer)
+            print(' ')
+            pc_history.append(answer)
+            current_index2 =+ r
+
+plaing_player_comp(current_index2)
+
+
+print('--------------------------------------------------------')
+print('Рассказ ПК: ')
+print('Меня зовут', pc_history[0], 'я живу в', pc_history[1], 'я занимаюсь', pc_history[2], 'мое хобби это', pc_history[3], 'мое домашнее животное это', 
+      pc_history[4])
+print('--------------------------------------------------------') '''
